532b646d711d217b2958dda0a813436f527de919
frooh0 <128101961+frooh0@users.noreply.github.com>
2024-04-16 10:03:35 -0700

    restructure

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,110 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerController : MonoBehaviour
 {
-    bool isMoving;
-    private float timeToMove = 0.2f;
-    private Vector2 posA, posB;
-    public string direction;
+    //movement logic
+    private Rigidbody2D rb;
     private BoxCollider2D collider;
-    [SerializeField] float distance = 0.5f;
-    [SerializeField] private float moveSpeed = 2f;
-    [SerializeField] private LayerMask Enemy;
-    //RaycastHit2D hit;
-    //Vector2 currentDirection;
+    private Vector2 moveInput;
+    private bool isMoving;
+    private Vector2 posA;
+    private Vector2 posB;
+    private float moveX;
+    private float moveY;
+    private string direction;
+    [SerializeField] private float moveSpeed = 5f;
+
+
+    //animation
+    private enum MovementState 
+    {
+        idleDown,
+        runDown
+    }
+    private Animator anim;
+    private SpriteRenderer sprite;
     
-    // Start is called before the first frame update
+
     void Start()
     {
-        //isMoving = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        var input = Input.inputString;
-        while(isMoving)
-        return;
-
-        if(!string.IsNullOrEmpty(input))
-        {
-            Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            //Debug.Log("Pressed char: " + Input.inputString);
-            //Debug.Log("Pressed char: " + input);
-            switch(input)
-            {
-                case "w":
-                    StartCoroutine(MovePlayer(Vector2.up));
-                    direction = "up";
-                    break;
-                case "a":
-                    StartCoroutine(MovePlayer(Vector2.left));
-                    direction = "left";
-                    break;
-                case "s":
-                    //currentDirection = Vector2.down;
-                    StartCoroutine(MovePlayer(Vector2.down));
-                    direction = "down";
-                    break;
-                case "d":
-                    StartCoroutine(MovePlayer(Vector2.right));
-                    direction = "right";
-                    break;
-            }
-
-            if(Input.GetKeyDown(KeyCode.Space) && RaycastAttack())
-            {
-                Debug.Log("Attack Success!");
-            }
-        }
-    }
-    private IEnumerator MovePlayer(Vector2 direction)
-    {
-        isMoving = true;
-        float elapsedTime = 0;
-
-        //point A to point B
-        posA = transform.position;
-        posB = posA + direction;
-
-        while(elapsedTime < timeToMove)
-        {
-            transform.position = Vector2.Lerp(posA, posB, (elapsedTime / timeToMove) * moveSpeed);
-            elapsedTime += Time.deltaTime;
-            
-            yield return 0;
-        }
-        transform.position = posB;
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
         isMoving = false;
     }
 
-    private bool RaycastAttack()
+    void OnMove(InputValue moveValue)
     {
-        //Debug.Log("Cast!");
-        switch(direction)
-        {
-            case "down":
-            Debug.Log("Casted Down!");
-            return Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, distance, Enemy);
-            default:
-            return false;
-        }        
+        moveInput = moveValue.Get<Vector2>();
     }
 
-    // private void FixedUpdate()
-    // {
-    //     hit = Physics2D.Raycast(transform.position, currentDirection, distance);
-    //     if(hit.collider != null)
-    //     {
-    //         Debug.DrawRay(transform.position, hit.point, Color.white);
-    //         Debug.Log("Enemy hit");
-    //     }
-    //     else
-    //     {
-    //         Debug.DrawRay(transform.position, transform.position + transform.right * distance, Color.black);
-    //     }
-    // }
+    void FixedUpdate()
+    {
+        Vector2 move = moveInput.normalized * moveSpeed * Time.fixedDeltaTime;
+        posA = transform.position;
+        posB = rb.position + move;
+        moveX = Input.GetAxisRaw("Horitzontal"); //remove
+        moveY = Input.GetAxisRaw("Vertical"); //remove
+        //find some other way to record direction, this input throws errors
+
+        if (moveY < 0f)
+        {
+            direction = "down";
+        }
+
+        if(!isMoving)
+        {
+            isMoving = true;
+            rb.MovePosition(rb.position + move);
+            isMoving = false;
+        }
+
+        AnimationUpdate();
+    }
+
+    void AnimationUpdate()
+    {
+        MovementState state;
+
+        switch (direction)
+        {
+            case "down":
+            state = MovementState.runDown;
+            anim.SetBool("runDown", true);
+            break;
+            case "default":
+            break;
+        }
+        
+
+        
+    }
 }
